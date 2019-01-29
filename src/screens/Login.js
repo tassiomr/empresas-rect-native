939ddec80486215cb8cd09dfafe6031107a83f97
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-29 20:00:43 -0200

    Tela de login

---
+++ src/screens/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ContainerView } from '../components;
+
+
+class Login extends React.Component { 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    onHandleLogin = () => {}
+
+    render() {
+        return <ContainerView></ContainerView>
+    }
+}

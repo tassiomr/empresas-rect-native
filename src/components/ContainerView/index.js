f12e11c98eb669cfbd16ac4b4fd2e936315fe34b
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-29 22:21:19 -0200

    containerview finalizado

--- src/components/ContainerView/index.js
+++ src/components/ContainerView/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, StyleSheet } from 'react-native';
 
 export const ContainerView = ({ color, children }) => (
-    <View style={{ flex: 1, backgroundColor: color }}>
+    <View style={[
+            styles.container,
+            { backgroundColor: color }]}
+    >
         <StatusBar backgroundColor={color} />
         { children }
     </View>
 )
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20
+    }
+})

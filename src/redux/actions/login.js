266abb409789c092740464659c0aee358fbc59b7
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-29 23:29:25 -0200

    tryLogin function

--- src/redux/actions/login.js
+++ src/redux/actions/login.js
@@ -6,13 +6,21 @@ export const FAILURE_LOGIN = 'FAILURE_LOGIN';
 
 
 const requestLogin = () => ({ type: REQUEST_LOGIN });
-const successLogin = () => ({ type: SUCCESS_LOGIN });
+const successLogin = data => ({ type: SUCCESS_LOGIN, data });
 const failureLogin = error => ({ type: FAILURE_LOGIN, error })
 
-export const tryLogin = (username, password) => dispatch => {
+const url = `http://empresas.ioasys.com.br/api/v1/users/auth/sign_in`
+
+export const tryLogin = (email, password) => dispatch => {
     try {
         dispatch(requestLogin());
-        axios.post();
+
+        axios.post(
+            url, 
+            { email, password }, 
+            { headers: { 'Content-Type': 'application/json'}}
+        ).then(e => dispatch(successLogin(e.headers)))
+            .catch(e => alert(e))
         dispatch(successLogin())
     } catch(error) {
         dispatch(failureLogin(error));

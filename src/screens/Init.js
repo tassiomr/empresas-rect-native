b43e22745fb653efa910340019897164a940d89d
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-29 22:38:41 -0200

    init page

---
+++ src/screens/Init.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ContainerView, Input, Button } from '../components'
+
+export default class Init extends React.Component { 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    onHandleLogin = () => {}
+
+    render() {
+        return (
+            <ContainerView color="tomato">
+                <Input 
+                    placeholder="Username" 
+                    value={this.state.username}
+                    onChangeText={text => this.setState({ username: text })}
+                />
+                <Input 
+                    placeholder="Password" 
+                    value={this.state.username}
+                    onChangeText={text => this.setState({ username: text })}
+                />
+                <Button 
+                    label="Login"
+                    color="cyan"
+                    onPress={() => {}}
+                />
+            </ContainerView>
+        )
+    }
+}

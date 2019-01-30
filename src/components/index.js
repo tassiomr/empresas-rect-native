5e48d061b86e3ee1bd21199a858a5fceff765b5e
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-30 00:54:41 -0200

    export wrapper

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { ContainerView } from './ContainerView';
 export { Input } from './Input';
 export { ActivityIndicatorView } from './ActivityIndicatorView';
 export { Button } from './Button';
+export { Wrapper } from './Wrapper';

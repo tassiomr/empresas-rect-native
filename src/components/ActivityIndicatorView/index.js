a54ba87c907b333ca81363f4bdfb095947ed2b50
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-29 22:01:23 -0200

    activityindicatorview

---
+++ src/components/ActivityIndicatorView/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+
+export const ActivityIndicatorView = () => {
+    <View style={{ flex: 1 }}>
+        <ActivityIndicator size="large" color="tomato" />
+    </View>
+}

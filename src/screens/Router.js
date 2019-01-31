991970b58fa217a98f2e702f15eb47d7c44a8af9
Tássio Rocha <mm-01@MBK-55.queimadiaria.local>
2019-01-31 18:11:19 -0200

    adicionada rotas da aplicação

--- src/screens/Router.js
+++ src/screens/Router.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
 import Home from './Home';
+import Login from './Login';
 
 import store from '../redux/store';
 
+const AppNavigator = createStackNavigator(
+  {
+    Login,
+    Home,
+  },
+  {
+    initialRouteName: 'Login',
+  },
+);
+
+const Navigator = createAppContainer(AppNavigator);
+
 const Router = () => (
   <Provider store={store}>
-    <Home />
+    <Navigator />
   </Provider>
 );
 

82b200d6c32fd42882c77b3c2b380cfde2ceadd8
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-31 00:58:42 -0200

    adicionado event error

--- src/components/ContainerView/index.js
+++ src/components/ContainerView/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View, StatusBar, StyleSheet } from 'react-native';
+import { Title } from '../Titles';
 
-export const ContainerView = ({ color, children }) => (
+export const ContainerView = ({ color, children, error }) => (
     <View style={[
             styles.container,
             { backgroundColor: color }]}
     >
         <StatusBar backgroundColor={color} barStyle="dark-content" />
         { children }
+        {
+            error 
+            ? <View style={{ height: 50, backgroundColor: 'yellow', justifyContent: "center", alignItems: 'center' }}><Title>{error}</Title></View>
+            : null
+        }
     </View>
 )
 

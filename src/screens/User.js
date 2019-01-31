d950f8863fcd57ce78c8f94644dba8f641b85901
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-31 00:36:51 -0200

    refactor user

--- src/screens/User.js
+++ src/screens/User.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, AsyncStorage } from 'react-native';
+import { Animated, View, AsyncStorage } from 'react-native';
 import { Title, SubtTitle } from '../components/Titles';
 import { Button } from '../components';
 
 export default class User extends React.Component {
     state = {
+        animateOpacity: new Animated.Value(0),
         user: {
             investor: {
                 investor_name: '',
@@ -15,29 +16,38 @@ export default class User extends React.Component {
         }
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const user = await AsyncStorage.getItem('user');
         this.setState({ user: JSON.parse(user) })
+        Animated.timing(
+            this.state.animateOpacity,
+            {
+                toValue: 1,
+                duration: 900
+            }
+        ).start()
     }
 
     render () {
-        const { user } = this.state;
+        const { user, animateOpacity } = this.state;
         return (
-            <View style={{ 
+            <Animated.View style={{ 
                 position: 'absolute' ,  
                 backgroundColor: 'rgba(0,0,0,0.4)', 
                 height: '100%', 
-                alignItems: 'flex-end',
+                opacity: animateOpacity,
+                alignItems: 'fssslex-end',
                 width: '100%' }}>
-                <View style={{ 
+                <Animated.View style={{ 
                     top: '9%',
                     paddingTop: 5,
                     marginRight: 40,
                     backgroundColor: 'white', 
                     borderRadius: 5,
+                    opacity: animateOpacity,
                     height: '30%',
                     width: '65%' }}>
-                    <View style={{ paddingLeft: 15, flex: 1 }}>
+                    <Animated.View style={{ opacity: animateOpacity, paddingLeft: 15, flex: 1 }}>
                         <Title>
                            { user.investor.investor_name } 
                         </Title>
@@ -50,10 +60,10 @@ export default class User extends React.Component {
                         <SubtTitle>
                             Country { user.investor.country }
                         </SubtTitle>
-                    </View>
+                    </Animated.View>
                     <Button label="Logout" color="cyan"/>
-                </View>
-            </View>
+                </Animated.View>
+            </Animated.View>
         )
     }
 }

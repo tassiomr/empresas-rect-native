bd14ba7454dfbc81bb653d70628e58b495a417fe
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-30 23:32:14 -0200

    getAllEnterprises

--- src/redux/actions/enterprise.js
+++ src/redux/actions/enterprise.js
@@ -4,11 +4,38 @@ import { AsyncStorage } from 'react-native';
 const url = `http://empresas.ioasys.com.br/api/v1`;
 
 
-const getAllEnterpresises = () => dispatch => {
+export const REQUEST_GET_ALL = 'REQUEST_GET_ALL';
+export const SUCCESS_GET_ALL = 'SUCCESS_GET_ALL';
+export const FAILURE_GET_ALL = 'FAILURE_GET_ALL';
+
+const requestGetAll = () => ({ type: REQUEST_GET_ALL });
+const successGetAll = data => ({ type: SUCCESS_GET_ALL, data })
+const failureGetAll = error => ({ type: FAILURE_GET_ALL, error })
+
+export const getAllEnterpresises = () => async dispatch => {
     try{
-        const accessToken = AsyncStorage.getItem('access-token');
-        const client = AsyncStorage.getItem('client');
-        const uid = AsyncStorage.getItem('uid');
+        dispatch(requestGetAll())
+        const accessToken = await AsyncStorage.getItem('accessToken');
+        const client = await AsyncStorage.getItem('client');
+        const uid = await AsyncStorage.getItem('uid');
+
+        const result = await axios.get(`${url}/enterprises/`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'access-token': accessToken,
+                client,
+                uid
+            }
+        })
+
+        if(result.error) {
+            dispatch(failureGetAll(result.error))
+        } else {
+            AsyncStorage.setItem('enterprises', JSON.stringify(result.data));
+            dispatch(successGetAll(result.data));
+        }
+        
+        
     }
     catch(error) {}
 }

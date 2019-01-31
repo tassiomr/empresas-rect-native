35ad9292bdffc553d62ffb303406053da5ec6669
Tassio Marcos <tassiomarcosr@outlook.com>
2019-01-31 00:59:27 -0200

    adicionado retorno de erro

--- src/redux/reducers/login.js
+++ src/redux/reducers/login.js
@@ -1,7 +1,8 @@
 import { FAILURE_LOGIN, SUCCESS_LOGIN, REQUEST_LOGIN, REQUEST_SET_CREDENTIALS, SUCCESS_SET_CREDENTIALS, FAILURE_SET_CREDENTIALS } from "../actions/login";
 
 const initalState = {
-    loading: false
+    loading: false,
+    error: null
 }
 
 
@@ -9,11 +10,11 @@ export default (state = initalState, action) => {
 
     switch(action.type) {
         case REQUEST_LOGIN:
-            return { ...state, loading: true, type: action.type }
+            return { ...state, loading: true, type: action.type, error: null }
         case FAILURE_LOGIN:
             return { ...state, loading: false, type: action.type, error: action.error }
         case SUCCESS_LOGIN:
-            return { ...state, loading: false, type: action.type, data: action.data, user: action.user }
+            return { ...state, loading: false, type: action.type, data: action.data, user: action.user,  }
         case REQUEST_SET_CREDENTIALS:
             return { ...state, loading: true, type: action.type, }
         case SUCCESS_SET_CREDENTIALS:
